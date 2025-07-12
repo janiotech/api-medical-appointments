5969d52ea8cd9dfc88a3b65a00e5d39f3d522a0b
jânio <sk8.merp@gmail.com>
2025-07-11 21:02:01 -0300

    chore: created project

---
+++ src/main/java/tech/janio/medical_appointments/MedicalAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package tech.janio.medical_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentsApplication.class, args);
+	}
+
+}
