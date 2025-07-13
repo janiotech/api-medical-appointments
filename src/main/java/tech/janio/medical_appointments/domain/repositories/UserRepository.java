b8473aa756852eb6c719e3e78a262f04a8a95492
jânio <sk8.merp@gmail.com>
2025-07-13 17:42:40 -0300

    feat: domain repository UserRepository created

---
+++ src/main/java/tech/janio/medical_appointments/domain/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package tech.janio.medical_appointments.domain.repositories;
+
+import tech.janio.medical_appointments.domain.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    User save(User user);
+    Optional<User> findById(UUID id);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
