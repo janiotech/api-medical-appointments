37fcb857e2d0dca5a67c8172d0fe66db57cf16b5
jânio <sk8.merp@gmail.com>
2025-07-18 12:12:49 -0300

    feat: contructors created

--- src/main/java/tech/janio/medical_appointments/infrastructure/entity/UserEntity.java
+++ src/main/java/tech/janio/medical_appointments/infrastructure/entity/UserEntity.java
@@ -31,6 +31,20 @@ public class UserEntity {
     private OffsetDateTime createdAt;
     private OffsetDateTime updatedAt;
 
+    public UserEntity(UUID id, String name, String email, String passwordHash, Set<Role> roles, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = passwordHash;
+        this.roles = roles;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UserEntity() {
+
+    }
+
     public UUID getId() { return id; }
     public void setId(UUID id) { this.id = id; }
 
