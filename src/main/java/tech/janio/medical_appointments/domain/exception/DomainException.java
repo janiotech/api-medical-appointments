6c8156f19343b41e233ba4859b08d968ef63aef3
jânio <sk8.merp@gmail.com>
2025-07-25 13:00:03 -0300

    feat: exception DomainException created

---
+++ src/main/java/tech/janio/medical_appointments/domain/exception/DomainException.java
@@ -0,0 +1,7 @@
+package tech.janio.medical_appointments.domain.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+}
