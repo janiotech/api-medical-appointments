8b2acc1711044ff8bf1a1115d328998155544c30
jânio <sk8.merp@gmail.com>
2025-08-01 11:43:31 -0300

    feat: add bean GetUsersPaginatedUseCase

--- src/main/java/tech/janio/medical_appointments/infrastructure/config/UserUseCaseConfig.java
+++ src/main/java/tech/janio/medical_appointments/infrastructure/config/UserUseCaseConfig.java
@@ -2,8 +2,9 @@ package tech.janio.medical_appointments.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tech.janio.medical_appointments.application.usecase.CreateUserUseCase;
-import tech.janio.medical_appointments.application.usecase.GetUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.CreateUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.GetUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.GetUsersPaginatedUseCase;
 import tech.janio.medical_appointments.domain.repository.UserRepository;
 
 @Configuration
@@ -15,5 +16,10 @@ public class UserUseCaseConfig {
     @Bean
     public GetUserUseCase getUserUseCase(UserRepository userRepository) {
         return new GetUserUseCase(userRepository);
+    }
+
+    @Bean
+    public GetUsersPaginatedUseCase getUsersPaginatedUseCase(UserRepository userRepository){
+        return new GetUsersPaginatedUseCase(userRepository);
     }
 }
