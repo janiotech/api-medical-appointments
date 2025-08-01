96c39305524933cfd95a51c6927a848945a90c8b
jânio <sk8.merp@gmail.com>
2025-08-01 11:46:35 -0300

    feat: add route, search users by page

--- src/main/java/tech/janio/medical_appointments/infrastructure/controller/UserController.java
+++ src/main/java/tech/janio/medical_appointments/infrastructure/controller/UserController.java
@@ -2,11 +2,12 @@ package tech.janio.medical_appointments.infrastructure.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tech.janio.medical_appointments.application.usecase.CreateUserUseCase;
-import tech.janio.medical_appointments.application.usecase.GetUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.CreateUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.GetUserUseCase;
+import tech.janio.medical_appointments.application.usecase.user.GetUsersPaginatedUseCase;
 import tech.janio.medical_appointments.domain.model.User;
-import tech.janio.medical_appointments.infrastructure.dto.CreateUserRequest;
-import tech.janio.medical_appointments.infrastructure.dto.UserResponse;
+import tech.janio.medical_appointments.application.dto.user.UserRequest;
+import tech.janio.medical_appointments.application.dto.user.UserResponse;
 
 import java.net.URI;
 import java.util.UUID;
@@ -16,14 +17,16 @@ import java.util.UUID;
 public class UserController {
     private final CreateUserUseCase createUserUseCase;
     private final GetUserUseCase getUserUseCase;
+    private final GetUsersPaginatedUseCase getUsersPaginatedUseCase;
 
-    public UserController(CreateUserUseCase createUserUseCase, GetUserUseCase getUserUseCase) {
+    public UserController(CreateUserUseCase createUserUseCase, GetUserUseCase getUserUseCase, GetUsersPaginatedUseCase getUsersPaginatedUseCase) {
         this.createUserUseCase = createUserUseCase;
         this.getUserUseCase = getUserUseCase;
+        this.getUsersPaginatedUseCase = getUsersPaginatedUseCase;
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
         User user = createUserUseCase.execute(request.toDomain());
         UserResponse response = UserResponse.fromDomain(user);
 
@@ -40,5 +43,12 @@ public class UserController {
         }
         UserResponse response = UserResponse.fromDomain(user);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public Object PaginatedResponsegetAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return getUsersPaginatedUseCase.execute(page, size);
     }
 }
