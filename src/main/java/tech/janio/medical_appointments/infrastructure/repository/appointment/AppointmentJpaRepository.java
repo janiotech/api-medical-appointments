b031d7f8635f96721d852deb08819ace136aa606
jânio <sk8.merp@gmail.com>
2025-08-01 11:24:09 -0300

    feat: deleting to remake

--- src/main/java/tech/janio/medical_appointments/infrastructure/repository/appointment/AppointmentJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.janio.medical_appointments.infrastructure.repository.appointment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.janio.medical_appointments.infrastructure.entity.AppointmentEntity;
-
-import java.util.UUID;
-
-public interface AppointmentJpaRepository extends JpaRepository<AppointmentEntity, UUID> {
-}
