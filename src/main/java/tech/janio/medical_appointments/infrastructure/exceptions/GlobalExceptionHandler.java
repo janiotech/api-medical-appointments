5084f4f61ddc3fb4c2fc8630717ec8473b2a6302
jânio <janiotech@gmail.com>
2025-08-16 17:16:43 -0300

    feat: added standard global exceptions separated by context NoResourceFoundException and MethodArgumentTypeMismatchException

--- src/main/java/tech/janio/medical_appointments/infrastructure/exceptions/GlobalExceptionHandler.java
+++ src/main/java/tech/janio/medical_appointments/infrastructure/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package tech.janio.medical_appointments.infrastructure.exceptions;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import tech.janio.medical_appointments.domain.exceptions.DomainException;
 
 @RestControllerAdvice
@@ -23,11 +27,18 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse("duplicate_user", "A user with this email already exists."));
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUnexpected(Exception ex) {
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException ex) {
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("500", "internal server error"));
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse("404", "route not found"));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse("400", "invalid parameter type"));
     }
 
     public static class ErrorResponse {
